e20182fcb168b2fbb20293d80a71f3abd37f9e72
SajanThapa22 <sajanthapa1888@gmail.com>
2024-06-25 23:30:41 +0545

    broadcast section

--- admin/comment-edit.php
+++ admin/comment-edit.php
@@ -14,12 +14,8 @@ else
 
 
     <title>FunOlympic  payris 2024</title>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
