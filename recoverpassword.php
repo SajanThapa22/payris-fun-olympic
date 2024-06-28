140f6a4dd6d2eb7ac30db17739a8bab097b48042
SajanThapa22 <sajanthapa1888@gmail.com>
2024-06-28 23:44:39 +0545

    password recovery

--- recoverpassword.php
+++ recoverpassword.php
@@ -9,28 +9,6 @@ include 'config/connection.php';
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
     <title>Olympic 2024</title>
-    <style>
-    .notification {
-        background-color: #f8f8f8;
-        border: 1px solid #ddd;
-        border-left: 5px solid #008374;
-        padding: 15px;
-        display: flex;
-        align-items: center;
-    }
-
-    .notification-icon {
-        margin-right: 12px;
-        font-size: 18px;
-        margin-bottom: 20px;
-        color: #808080;
-    }
-
-    .notification-text {
-        font-size: 17px;
-        color: #808080;
-    }
-    </style>
 </head>
 
 <body>
@@ -38,7 +16,7 @@ include 'config/connection.php';
 
 
     <header id="header" class="header d-flex align-items-center" >
-        <div class="container-fluid container-xl d-flex align-items-center justify-content-between" style="margin-top: 70px;">
+        <div style="margin-top: 70px;">
             <a href="index.php" class="logo d-flex align-items-center">
             </a>
         </div>
@@ -46,20 +24,19 @@ include 'config/connection.php';
 
     <br>
 
-    <main class="login-form">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-6">
-                    <div class="card" style="border-radius: 15px">
+    <main>
+        <div>
+            <div>
+                <div >
+                    <div style="border-radius: 15px">
                         <div style="padding: 15px; font-size: 18px;color:white;background-color:#dc2626;">
                             <strong>Reset Password</strong>
                         </div>
-                        <div class="card-body">
-                            <form action="#" method="POST" name="recoverpassword">
-                                <div class="form-group row">
-                                    <div class="notification">
-                                        <i class="fas fa-info-circle notification-icon"></i>
-                                        <span class="notification-text">
+                        <div>
+                            <form method="POST">
+                                <div>
+                                    <div >
+                                        <span >
                                             A verification mail has been sent to your email address. Please kindly check it.
                                         </span>
                                     </div>
@@ -67,7 +44,7 @@ include 'config/connection.php';
                                 <br>
                                 <div class="col-md-15">
                                     <label>Email</label>
-                                    <input type="text" id="email_address" class="form-control" style= "margin-top: 5px;" placeholder="Enter email address"
+                                    <input type="text" id="email_address" style= "margin-top: 5px;" placeholder="Enter email address"
                                         name="email" required autofocus>
                                 </div>
                         </div>
@@ -90,19 +67,19 @@ include 'config/connection.php';
 <?php 
     if(isset($_POST["recover"])){
         include 'config/connection.php';
-        $email = $_POST["email"];
+        $user_email = $_POST["email"];
 
-        $sql = mysqli_query($conn, "SELECT * FROM users WHERE email='$email'");
-        $query = mysqli_num_rows($sql);
-  	    $fetch = mysqli_fetch_assoc($sql);
+        $pfo_sql = mysqli_query($conn, "SELECT * FROM users WHERE email='$user_email'");
+        $query = mysqli_num_rows($pfo_sql);
+  	    $get = mysqli_fetch_assoc($pfo_sql);
 
-        if(mysqli_num_rows($sql) <= 0){
+        if(mysqli_num_rows($pfo_sql) <= 0){
             ?>
 <script>
 alert("<?php  echo "The email you provided doesn't exist"?>");
 </script>
 <?php
-        }else if($fetch["status"] == 0){
+        }else if($get["status"] == 0){
             ?>
 <script>
 alert("Sorry, your account must verify first, before you recover your password !");
@@ -110,10 +87,10 @@ window.location.replace("login.php");
 </script>
 <?php
         }else{
-            $token = bin2hex(random_bytes(50));
+            $pw_token = bin2hex(random_bytes(50));
 
-            $_SESSION['token'] = $token;
-            $_SESSION['email'] = $email;
+            $_SESSION['token'] = $pw_token;
+            $_SESSION['email'] = $user_email;
 
             require "Mail/phpmailer/PHPMailerAutoload.php";
             $mail = new PHPMailer;
@@ -131,15 +108,10 @@ window.location.replace("login.php");
             $mail->addAddress($_POST["email"]);
             $mail->addReplyTo('sajanthapa1888@gmail.com.com');
 
-            // HTML body
             $mail->isHTML(true);
             $mail->Subject="Recover your password";
             $mail->Body="<b>Dear User</b>
-            <h3>We received a request to reset your password.</h3>
-            <p>Kindly click the below link to reset your password</p>
-            http://localhost/productassignment/resetpassword.php
-            <br><br>
-            <p>With regrads,</p>";
+            <p>We just got notified to reset your password. please check your email to reset your password</p>
 
             if(!$mail->send()){
                 ?>
